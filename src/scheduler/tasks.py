95ba8a60511f5567d8373934b639d2e12f73af84
marhcouto <marhcouto1101@gmail.com>
2023-06-05 19:36:12 +0100

    fix: updated test_task

--- src/scheduler/tasks.py
+++ src/scheduler/tasks.py
@@ -1,13 +1,5 @@
-# import sys
-# import time
-import signal
-
-def interrupt_handler(signum, frame):
-    print("Handling signal {}".format(signum))
-
 
 def test_task():
-    signal.signal(signal.SIGINT, interrupt_handler)
     for i in range(100000000):
         if (i % 1000000 == 0):
             print("Task 1: {}".format(i))
